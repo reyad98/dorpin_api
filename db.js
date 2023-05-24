b9b5c747a6e3cee317ea3f0fc9ed8f391c4418f7
reyade <alexmarfi2233@gmail.com>
2023-05-24 06:52:35 -0600

    yuuu

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
-import mysql from "mysql2";
+import dotenv from "dotenv";
+import mysql from "mysql";
+
+dotenv.config(); // Load environment variables from .env file
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Anisur2233#",
-    database:"blog"
-
-
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
