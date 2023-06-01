5d0ee9abba0b60a82f2cd414cd2015c83139d1e7
reyade <alexmarfi2233@gmail.com>
2023-06-01 03:36:54 -0600

    34

--- index.js
+++ index.js
@@ -1,57 +1,40 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware/dist/index.js';
 import multer from "multer";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '../client/public/upload');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  }
-});
+    destination: function (req, file, cb) {
+      cb(null, '../client/public/upload')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now()+file.originalname)
+    }
+  })
 
-const upload = multer({ storage });
+const upload = multer({ storage })
 
 app.post('/api/upload', upload.single('file'), function (req, res) {
-  const file = req.file;
-  res.status(200).json(file.filename);
-});
-
-app.use(
-  '/api/auth',
-  createProxyMiddleware({
-    target: 'https://mmemories.onrender.com',
-    changeOrigin: true,
+    const file = req.file
+    res.status(200).json(file.filename)
+    
   })
-);
-app.use(
-  '/api/users',
-  createProxyMiddleware({
-    target: 'https://mmemories.onrender.com',
-    changeOrigin: true,
-  })
-);
-app.use(
-  '/api/posts',
-  createProxyMiddleware({
-    target: 'https://mmemories.onrender.com',
-    changeOrigin: true,
-  })
-);
 
-app.listen(8800, () => {
-  console.log('Connected!');
-});
+app.use("/api/auth",  authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
 
-app.get('/', (req, res) => {
-  res.send('Backend Conn!!!!!');
-});
+app.listen(8800,()=>{
+    console.log("Connected!")
+})
 
-
+app.get('/',(req,res)=>{
+  res.send("Backend Conn!!!!!")
+})
