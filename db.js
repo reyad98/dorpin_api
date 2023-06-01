57d23b2337ef44ccfd97e29ea168b7c7b225486d
reyade <alexmarfi2233@gmail.com>
2023-05-31 23:57:49 -0600

    re

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
+import dotenv from "dotenv";
 import mysql from "mysql";
 
+dotenv.config(); // Load the environment variables from the .env file
+
+const port = process.env.PORT || 8800;
+
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "12345",
-  database: "blog",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || port,
 });
