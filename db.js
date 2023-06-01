7142794b674c9d2214c8cc406ea46a5d3c68861f
reyade <alexmarfi2233@gmail.com>
2023-06-01 00:21:37 -0600

    11

--- db.js
+++ db.js
@@ -1,14 +1,26 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
+// import mysql from "mysql";
+
+// dotenv.config(); // Load the environment variables from the .env file
+
+// export const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   port: process.env.DB_PORT || 8800,
+// });
+
+
 import mysql from "mysql";
 
-dotenv.config(); // Load the environment variables from the .env file
-
-const port = process.env.PORT || 8800;
+const port = process.env.DB_PORT || 8800;
 
 export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT || port,
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "blog",
+  port: process.env.NODE_ENV === "production" ? port : undefined,
 });
+
